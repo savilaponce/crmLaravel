0a663096cc19e98ff5c0405a78e652d125282c48
savilaponce <samuelavilaponce2006@alumnos.alborfp.com>
2026-02-16 18:51:00 +0100

    cambios de flujo en el login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Añade estas líneas aquí abajo:
+        $middleware->redirectTo(
+            guests: '/login',
+            users: '/'
+        );
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
